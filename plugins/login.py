65768cf878fb69e7f0ae6fd75671ad9643ba0520
ZQHD <zcr20080901@163.com>
2024-04-27 13:42:34 +0800

    编写封禁处罚函数，加入时间格式化

--- plugins/login.py
+++ plugins/login.py
@@ -1,7 +1,8 @@
-from mcdreforged.api.command import SimpleCommandBuilder
+from mcdreforged.api.command import *
 from mcdreforged.info_reactor.info import Info
 from mcdreforged.minecraft.rcon.rcon_connection import RconConnection
 import time
+import requests
 PLUGIN_METADATA = {
     'id': 'login',
     'version': '1.0.0',
@@ -12,14 +13,21 @@ PLUGIN_METADATA = {
 
 }
 rcon = RconConnection("127.0.0.123",38324,"123456")
+userKeys = ""
+serverName = ""
+name = ""
+password = ""
 def on_load(server, old):
     buider = SimpleCommandBuilder()
-    # buider.command("mpwa bind",bind)
+    buider.command("mpwa ban <name> <description>",ban)
+
+    buider.arg("name",Text)
+    buider.arg("description",Text)
     server.logger.info('Hello world!')
     buider.register(server)
 def on_user_info(server, info:Info):
     if info.content == 'mpwa bind':
         user = info.player
         server.reply(info, '绑定开始！用户名：{}，时间：{}'.format(user,time.time()))
-# def bind():
-#     print("succeed")
+def ban():
+    print("succeed")
